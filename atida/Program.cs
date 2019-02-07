c28b1711221e71d52fe6c169acfa51b1569a5ad9
Tendai Chikake <xulwrk@gmail.com>
2019-02-07 23:40:47 +0300

    add TOI and MathNet dependences

---
+++ atida/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using TOI;
+
+namespace atida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lab1 lab1 = new lab1();
+            lab2 lab2 = new lab2();
+            //lab 3 and lab 4 are static
+            Lab5 lab5 = new Lab5();
+            lab6 lab = new lab6();
+            //here use the functions defined in the labs classes to do what you desire. 
+        }
+    }
+}
